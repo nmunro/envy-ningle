2b1800a83c1f8b474010432fc50916bae004adfb
NMunro <nmunro@duck.com>
2025-05-11 20:39:58 +0100

    Added function to work with migrations

--- src/main.lisp
+++ src/main.lisp
@@ -1,8 +1,17 @@
 (defpackage envy-ningle
   (:use :cl)
-  (:export #:build-middleware))
+  (:export #:build-middleware
+           #:extract-mito-config))
 
 (in-package envy-ningle)
+
+(defun extract-mito-config (env)
+  "Returns the keyword backend and initargs from :mito middleware in the ENVY config."
+  (let ((middleware (getf (envy:config env) :middleware)))
+    (loop :for mw :in middleware
+          :when (and (consp mw) (eq (car mw) :mito))
+          :return (destructuring-bind (_ (backend &rest args)) mw
+                   (values backend args)))))
 
 (defun build-middleware (envy-config app)
   "Wraps the given APP in middleware as defined in ENVY config at runtime."
