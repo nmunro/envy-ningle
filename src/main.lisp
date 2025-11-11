557693a7101c665262327457ec94af4e50d931e5
NMunro <neilmunro@gmail.com>
2025-11-11 07:53:07 +0000

    Simplified middleware quoting

--- src/main.lisp
+++ src/main.lisp
@@ -35,7 +35,7 @@
     (handler-case
         ;; This ensures each middleware entry is quoted
         (eval `(lack.builder:builder
-                 ,@(mapcar (lambda (entry) `',entry) middleware)
+                 ,@middleware
                  ,app))
       (error (e)
         (format *error-output* "~&[build-middleware ERROR] ~A~%" e)
